2210ff2b2bf55888ce3486307f79bc2a7f4182c1
Rareș-Gabriel Mușea <77101390+RaresMusea@users.noreply.github.com>
2025-03-22 14:11:58 +0200

    Provide backend functionality for creating files and dirs (#2)

--- src/ws.ts
+++ src/ws.ts
@@ -2,7 +2,7 @@ import { Server, Socket } from "socket.io";
 import { Server as HttpServer } from "http";
 import { fetchS3Folder, renameS3Directory, renameS3File, saveToS3 } from "./aws";
 import path from "path";
-import { deleteFile, fetchDir, fetchFileContent, saveFile } from "./fs";
+import { deleteFile, fetchDir, fetchFileContent, fileExists, saveFile } from "./fs";
 import fs from "fs/promises";
 import { TerminalManager } from "./pty";
 import { handleRenameError } from "./error_handler";
@@ -63,6 +63,37 @@ function initHandlers(socket: Socket, replId: string) {
         const contents = await fetchDir(dirPath, dir);
         console.log(contents);
         callback(contents);
+    });
+
+    socket.on("createFile", async ({ newName, parentDir }: { newName: string, parentDir: string }) => {
+        const absoluteNewFilePath: string = path.join(__dirname, `../tmp/${replId}${parentDir}/${newName}`);
+
+        if (fileExists(absoluteNewFilePath)) {
+            console.error(`[ERROR]: File ${newName} already exists in ${parentDir}!`);
+            socket.emit('fileCreationFailed', {
+                message: "Unable to create file",
+                description: `File ${newName} already exists in ${parentDir}!`
+            });
+            return;
+        }
+
+        try {
+            await saveFile(absoluteNewFilePath, '');
+            await saveToS3(`${parentDir}/${newName}`, '');
+            console.info(`[INFO]: Created file ${newName} in ${parentDir}.`);
+
+            socket.emit('fileCreated', {
+                name: newName,
+                parentDir: parentDir,
+                path: `${parentDir}/${newName}`
+            });
+        }
+        catch (error) {
+            socket.emit('fileCreationFailed', {
+                message: "Unable to create file",
+                description: `An error occurred while attempting to create file ${newName}!`
+            });
+        }
     });
 
     socket.on("renameEntity", async (data) => {
