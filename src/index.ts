fff21a1fc51cdbfba431833b6ef860f56dd63431
Rares Musea <raresmusea001@gmail.com>
2025-03-29 01:22:51 +0200

    Update express configuration for handling json responses

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,15 @@ import { initWs } from "./ws";
 import { initHttp } from "./http";
 import cors from "cors";
 import { logger } from "./logger";
+import projectRoutes from '../routes/projects';
 
 const app = express();
 
 logger.debug('App started.');
 
 app.use(cors());
+app.use(express.json());
+app.use('/project', projectRoutes);
 const httpServer = createServer(app);
 
 try {
